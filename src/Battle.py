ec6e676dff935f37e003063bc71bd5c220a20980
CMRiggs <cmriggs@ncsu.edu>
2022-09-24 14:39:50 -0400

    Added group cog acc and TU acc. Adjusted trackExp

--- src/Battle.py
+++ src/Battle.py
@@ -1,6 +1,7 @@
 from math import floor, ceil
+from re import T
 
-from Gags import *
+from GagTrack import *
 from Cog import Cog, getCogDefense
 
 class Battle:
@@ -23,8 +24,9 @@ def singleCogAccuracy(cog, gags, idxs, prevHits):
     # Go through every gag being used and find min propAcc and max trackExp
     for i,v in enumerate(idxs):
         propAcc = min(gags[i].accuracy[v], propAcc)
-        for j,b in enumerate(gags[i].expMilestones):
-            if (b >= gags[i].exp):
+        # Search list from top to bottom keeping original indexes
+        for j,b in reversed(list(enumerate(gags[i].expMilestones))):
+            if (b <= gags[i].exp):
                 trackExp = max(j*10,trackExp)
                 break
 
@@ -40,8 +42,59 @@ def singleCogAccuracy(cog, gags, idxs, prevHits):
         return 5
     return atkAcc
     
-def groupCogAccuracy(cogs, gags):
-    pass
+def groupCogAccuracy(cogs, gags, idxs, prevHits):
+    # Lowest propAcc is selected among all gags
+    propAcc = 100
+    # Highest trackExp bonus is selected among all gags
+    trackExp = 0
+
+    # Go through every gag being used and find min propAcc and max trackExp
+    for i,v in enumerate(idxs):
+        propAcc = min(gags[i].accuracy[v], propAcc)
+        # Search list top from bottom keeping original indexes
+        for j,b in reversed(list(enumerate(gags[i].expMilestones))):
+            if (b <= gags[i].exp):
+                trackExp = max(j*10,trackExp)
+                break
+
+    tgtDef = 0
+    cogsLured = 0
+    for _,v in enumerate(cogs):
+        tgtDef = min(getCogDefense(v.rank, v.level), tgtDef)
+        if v.lured == True:
+            cogsLured += 1
+    # No lure ratio bonus because singleCog
+    bonus = prevHits * 20
+
+    level7TS = False # Check for level 7 throw/squirt
+    for i,v in enumerate(gags):
+        if ((v.type == "Throw") or (v.type == "Squirt")) and (idxs[i] == 6):
+            level7TS = True
+            
+    # Lure ratio applies to sound and level 7 throw/squirt
+    if (gags[0].type == "Sound") or (level7TS):
+        bonus += (cogsLured/len(cogs))*100
+
+    atkAcc = propAcc + trackExp + tgtDef + bonus
+
+    # Max and min accuracy
+    if atkAcc > 95:
+        return 95
+    if atkAcc < 5:
+        return 5
+    return int(atkAcc)
+
+def toonUpAccuracy(gag, idx):
+    propAcc = gag.accuracy[idx]
+    for i,v in enumerate(gag.expMilestones):
+        if (v >= gag.exp):
+            trackExp = i*5
+    atkAcc = propAcc + trackExp
+    if atkAcc > 95:
+        return 95
+    if atkAcc < 5:
+        return 5
+    return atkAcc
 
 def lureDamage(damage):
     """Lured cogs take 1.5 damage from throw and squirt, this check
@@ -86,3 +139,5 @@ def damageCalculation(gag, idx):
     #    bonus = 1
     #damage += bonus
     #return damage
+
+print(groupCogAccuracy([Cog("Flunky", 5), Cog("Robber Baron", 12)], [Sound(4578, False)], [5], 0))
