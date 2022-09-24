897267f0644cd7d89c4278429d8eafc061efea15
CMRiggs <cmriggs@ncsu.edu>
2022-09-23 23:10:04 -0400

    Added clarification to documentation

--- src/Battle.py
+++ src/Battle.py
@@ -1,5 +1,6 @@
 class Battle:
     def __init__(self, toons, cogs):
-        """Toons and Cogs are both arrays of up to 4 toons/cogs objects"""
+        """Toons and Cogs are both arrays of up to 4 toons/cogs objects.
+        Index 0 represents the far right position"""
         self.toons = toons
         self.cogs = cogs
